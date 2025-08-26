1bdf83b2c2f4823a9db3fba9c13d3c1dab1b65ca
unknown <aminpyss@example.com>
2025-08-26 21:16:00 +0330

    start of web-post

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(port, () => {
+    `listening to port {port}`
+})
+
